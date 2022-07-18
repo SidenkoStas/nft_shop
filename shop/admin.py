10b31390f94baf0cbc4f3f65eacca4934ceed855
Oknedis <soknedis@yandex.ru>
2022-07-18 16:54:14 +0300

    Добавлен шаблон сайта; Исправлены ссылки загрузки скриптов и стилей.

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from shop.models import Category, Item
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Item)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, verbose_name="category")
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=150, verbose_name="item_name")
+    description = models.TextField(verbose_name="description")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="price")
+    category = models.ForeignKey(to="Category", on_delete=models.CASCADE, verbose_name="category")
+
+    def __str__(self):
+        return f"{self.title}"
